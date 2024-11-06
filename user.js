12bda730400d9f69a0c8cd29770641f4278e8e83
Bharathi-316 <114898681+Bharathi-316@users.noreply.github.com>
2024-11-06 16:56:31 +0530

    routes

--- user.js
+++ user.js
@@ -1,45 +1,12 @@
-require("dotenv").config();
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const express= require("express");
+const { registerUser, loginUser,logoutUser,dumpUsers,verifyEmail} = require("../controllers/user");
+const authenticate = require('../authenticate');
+const router = express.Router();
 
-var UserSchema = new Schema({
-    firstname: {
-        type: String,
-        default: ''
-      },
-    lastname: {
-        type: String,
-        default: ''
-      },
-    email: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    isVerified: {
-      type: Boolean,
-      default: false
-    },
-    admin: {
-      type: Boolean,
-      default: false
-    },
-    password: {
-        type: String,
-        max: 20,
-        required: true
-      },
-    emailToken: {
-        type: String
-    },
-    tokens:[{
-        token:{
-          type:String
-        }
-      }]
-},{
-  timestamps: true
-});
+router.post("/signup",registerUser);
+router.get("/verify-email",verifyEmail);
+router.post("/login",authenticate.isVerifiedUser,loginUser);             
+router.get("/logout",authenticate.verifyUser,logoutUser);
+router.get("/dump",authenticate.verifyUser,authenticate.verifyAdmin,dumpUsers);  //only admin can dump all users
 
-
-module.exports = mongoose.model('User', UserSchema);
+module.exports =router;
